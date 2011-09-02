c80fe5db33fa5de33980cd5c359df3c0ba9500a1
Bryan Bibat <bry@bryanbibat.net>
2011-09-02 12:04:16 +0800

    add route names (p51)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 AlingnenaApp::Application.routes.draw do
   resources :debts
 
-  match 'products' => 'products#index', :via => :get
-  match 'products/:id' => 'products#show', :via => :get
+  match 'products' => 'products#index', :via => :get, :as => 'products'
+  match 'products/:id' => 'products#show', :via => :get, :as => 'product'
 
 end
