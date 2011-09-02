713d55bc107abd1d489a6adb58cf2ee7943acd64
Bryan Bibat <bry@bryanbibat.net>
2011-09-02 16:37:30 +0800

    add callback (p75)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name, :description
+
+  before_validation :assign_default_description
+
+  private
+
+    def assign_default_description
+      if description.blank?
+        self.description = name
+      end
+    end
 end
