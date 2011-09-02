4bfb30c356acd41b3b44eb7d5219c764fc258662
Bryan Bibat <bry@bryanbibat.net>
2011-09-02 14:52:52 +0800

    add delete functionality (p55)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,8 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    # destroy the specified record
+    @product = Product.find(params[:id])
+    @product.destroy
     redirect_to products_path
   end
 
