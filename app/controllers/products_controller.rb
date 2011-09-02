73cfe21aff32d7979c59370519cd35f3ad2f8442
Bryan Bibat <bry@bryanbibat.net>
2011-09-02 11:47:53 +0800

    initial code for list products (p44-45)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+
+  def index
+  end
+
   def show
     @product = Product.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AlingnenaApp::Application.routes.draw do
   resources :debts
 
+  match 'products' => 'products#index', :via => :get
   match 'products/:id' => 'products#show', :via => :get
 
 end
