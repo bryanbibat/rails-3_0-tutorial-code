49fef98781c8db1f7113a8547655cb777c28fcde
Bryan Bibat <bry@bryanbibat.net>
2011-09-02 16:35:15 +0800

    add resource-based RESTful routing (p73)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 AlingnenaApp::Application.routes.draw do
   resources :debts
 
-  match 'products' => 'products#index', :via => :get, :as => 'products'
-  match 'products' => 'products#create', :via => :post
-  match 'products/new' => 'products#new', :via => :get, :as => 'new_product'
-  match 'products/search' => 'products#search', :via => :get, :as => 'search_products'
-  match 'products/:id' => 'products#show', :via => :get, :as => 'product'
-  match 'products/:id' => 'products#destroy', :via => :delete
-  match 'products/:id' => 'products#update', :via => :put
-  match 'products/:id/edit' => 'products#edit', :via => :get, :as => 'edit_product'
+  resources :products do
+    collection do
+      get 'search'
+    end
+  end
 
 end
