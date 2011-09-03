3e18bddf75a4330f8280a0e98a0ceaafef1b401c
bry <bry@bryanbibat.net>
2011-09-04 02:11:58 +0800

    Add display purchase helper (p91)

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,9 @@
 module InvoicesHelper
+  def display_purchase(invoice)
+    unless invoice.purchase.nil?
+      invoice.purchase.description
+    else
+      "(no Purchase set)"
+    end
+  end
 end
