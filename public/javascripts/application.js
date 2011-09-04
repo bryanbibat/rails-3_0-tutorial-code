d3fe17e5f6842227a39e1aecd373633c5fb62d91
bry <bry@bryanbibat.net>
2011-09-04 15:29:18 +0800

    more fun with jquery (p137)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  $("#notice").text("hello world!");
+  $("li").css("color", "blue");
+  $("li").mouseover(function () {
+    $(this).css("color", "red");
+  }).mouseout(function() {
+    $(this).css("color", "black");
+  });
 });
