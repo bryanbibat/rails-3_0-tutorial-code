a2da4cabf73fc4f65063b2bcbb0b9e1d7b35a514
bry <bry@bryanbibat.net>
2011-09-04 15:28:26 +0800

    jquery hello world pt2 (p137)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  alert("hello world!");
+  $("#notice").text("hello world!");
 });
