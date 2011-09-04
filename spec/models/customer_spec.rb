90cb917c26187a3e3f17108fae82ee241e89e7fe
bry <bry@bryanbibat.net>
2011-09-04 17:20:16 +0800

    replace pending case with valid test case (p144)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new instance given valid attributes" do
+    Customer.create!(:name => 'John', :active => false)   # will throw error on failure
+  end
 end
