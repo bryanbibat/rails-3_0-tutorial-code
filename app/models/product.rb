f52803126afcde5a81f6647aa0191489e56482a3
bry <bry@bryanbibat.net>
2011-09-04 12:37:10 +0800

    has_many :through (p108-110)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name, :description
   has_many :line_items
+  has_many :purchases, :through => :line_items
 
   before_validation :assign_default_description
 
