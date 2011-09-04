ae9be18707845b39ff0edb3f6755c42414151862
bry <bry@bryanbibat.net>
2011-09-04 15:07:57 +0800

    Form submission via Ajax (p131-132)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,10 +31,23 @@ class MessagesController < ApplicationController
       if @message.save
         format.html { redirect_to(messages_path, :notice => 'Message was successfully created.') }
         format.xml  { render :xml => @message, :status => :created, :location => @message }
+        format.js do
+          render :update do |page|
+            page.replace_html :message_list,
+              :partial => Message.all(:order => "created_at DESC")
+            page.replace_html :notice, 'Message was successfully created.'
+            page[:new_message].reset
+          end
+        end
       else
         @messages = Message.order("created_at DESC")
         format.html { render :action => "index" }
         format.xml  { render :xml => @message.errors, :status => :unprocessable_entity }
+        format.js do
+          render :update do |page|
+            page.replace_html :notice, 'There was an error in creating the message.'
+          end
+        end
       end
     end
   end
